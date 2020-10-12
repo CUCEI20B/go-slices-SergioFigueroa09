0808bfc9817b7b2c6bfc425ed72ff2f213066c42
unknown <sergio.figueroa7875@alumnos.udg.mx>
2020-10-12 14:04:43 -0500

    Actividad de Slices V.01

--- main.go
+++ main.go
@@ -2,6 +2,17 @@ package main
 
 import "fmt"
 
-func main()  {
-	fmt.Println("Hola Mundo!") // eliminar esta línea
+func main(){
+	var s []int
+	var n int
+	var aux int
+	var total int = 0
+	fmt.Scan(&n)
+	for i:=0; i < n; i++{
+		fmt.Scan(&aux)
+		s = append(s, aux)
+		total = total + s[i]
+		//fmt.Println(v)
+	}
+	fmt.Println(total)
 }
